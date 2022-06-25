185e17f1c948f3c93a9dafb75ea3866f3b882a2d
Derlis Alejandra Cuartas <dcuartasa@unal.edu.co>
2022-06-25 14:33:48 -0500

    Lab 2- MR

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def purpose_column(x):
+    return  clear_spaces(x[1]) +  "*" + clear_spaces(x[0])
+
+if __name__=="__main__":
+    for line in sys.stdin:
+        result = line.split(',')
+        print(purpose_column(result))
--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def purpose_column(x):
+    return  clear_spaces(x[0]), clear_spaces(x[1]).split(",")
+
+if __name__=="__main__":
+    for line in sys.stdin:
+        line = line.replace("'","")
+        result = line.split('\t')
+        number, array = purpose_column(result) 
+        for letter_array in array:
+            print(letter_array + "*" + number)
